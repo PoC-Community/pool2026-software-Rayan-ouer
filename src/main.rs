77bc5a6001122c263252adc97ba0fe7d4b623ede
rayan <rayan.ouerdane@epitech.eu>
2026-02-02 14:32:20 +0100

    test task 3

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod get_even_numbers;
+mod what_is_your_name;
+
+use what_is_your_name::what_is_your_name;
 
 fn main() {
-    let array = vec![-1, -0, -19, -17, -16, -8, -13, -24];
-    println!("{}", get_even_numbers::get_even_numbers(&array));
+    println!("{}", what_is_your_name());
 }
