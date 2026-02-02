ca1eb9b120db524736945d5a8df63b6eba07f608
rayan <rayan.ouerdane@epitech.eu>
2026-02-02 14:37:56 +0100

    make get number more clear

--- src/get_even_numbers.rs
+++ src/get_even_numbers.rs
@@ -1,14 +1,19 @@
 use std::ops::Add;
 
-pub fn get_even_numbers(numbers: &[i32]) -> String
-{
-    let mut numbers_copy: Vec<_>= numbers.iter().filter(|&&x| (x % 2) == 0).collect();
+#[allow(dead_code)]
+pub fn get_even_numbers(numbers: &[i32]) -> String {
+    let mut numbers_copy: Vec<_> = numbers.iter().filter(|&&x| (x % 2) == 0).collect();
     numbers_copy.sort();
+    let mut i = 0;
+    let mut stuff_str: String = numbers_copy
+        .into_iter()
+        .map(|i| i.to_string().add(" - "))
+        .collect::<String>();
 
-    let mut stuff_str: String = numbers_copy.into_iter().map(|i| i.to_string().add(" - ")).collect::<String>();
-    stuff_str.pop();
-    stuff_str.pop();
-    stuff_str.pop();
+    while i < 3 {
+        stuff_str.pop();
+        i += 1;
+    }
     stuff_str
 }
 
